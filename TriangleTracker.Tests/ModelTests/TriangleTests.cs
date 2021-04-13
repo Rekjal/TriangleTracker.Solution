b190ca46c98c45f58681600fb45f1586f75070b0
Salim Mayan <mailsalim@gmail.com>
2021-04-12 19:08:32 -0700

    create boilerplate structure
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

---
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace Triangle.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        // [TestMethod]
+        // public void IsLeapYear_NumberDivisibleByFour_True()
+        // {
+        //     LeapYear testLeapYear = new LeapYear();
+        //     Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+        // }
+
+        // [TestMethod]
+        // public void IsLeapYear_NumberNotDivisibleByFour_False()
+        // {
+        //     LeapYear testLeapYear = new LeapYear();
+        //     Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+        // }
+
+        // [TestMethod]
+        // public void IsLeapYear_MultiplesOfOneHundred_False()
+        // {
+        //     LeapYear testLeapYear = new LeapYear();
+        //     Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+        // }
+
+        // [TestMethod]
+        // public void IsLeapYear_MultiplesOfFourHundred_True()
+        // {
+        //     LeapYear testLeapYear = new LeapYear();
+        //     Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+        // }
+
+    }
+}
