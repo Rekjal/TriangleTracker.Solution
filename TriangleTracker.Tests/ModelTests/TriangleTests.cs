3f3464a82d4edf487dca7e48193cafc11d92c409
Salim Mayan <mailsalim@gmail.com>
2021-04-12 21:03:01 -0700

    fix bug with tests (static method was being incorrectly called via object)
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -9,27 +9,30 @@ namespace TriangleTracker.Tests
         [TestMethod]
         public void TriangleChecker_CheckValidTriangle_True()
         {
-            Triangle testTriangle = new Triangle();
+            // Triangle testTriangle = new Triangle();
             string testResult = "Not A Triangle";
-            Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,3,8));
+            // Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,3,8));
+            Assert.AreEqual(testResult, Triangle.TriangleChecker(4,3,8));
         }
 
         [TestMethod]
 
         public void TriangleChecker_CheckEquilateral_True()
         {
-            Triangle testTriangle = new Triangle();
+            // Triangle testTriangle = new Triangle();
             string testResult = "Your triangle is an equilateral triangle";
-            Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,4,4));
+            // Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,4,4));
+            Assert.AreEqual(testResult, Triangle.TriangleChecker(4,4,4));
         }
 
         [TestMethod]
 
         public void TriangleChecker_CheckIsosceles_True()
         {
-            Triangle testTriangle = new Triangle();
+            // Triangle testTriangle = new Triangle();
             string testResult = "Your triangle is an isosceles triangle";
-            Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,4,7));
+            // Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,4,7));
+            Assert.AreEqual(testResult, Triangle.TriangleChecker(4,4,7));
         }
 
         [TestMethod]
@@ -37,9 +40,10 @@ namespace TriangleTracker.Tests
 
         public void TriangleChecker_CheckScalene_True()
         {
-            Triangle testTriangle = new Triangle();
+            // Triangle testTriangle = new Triangle();
             string testResult = "Your triangle is a scalene triangle";
-            Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,5,7));
+            // Assert.AreEqual(testResult, testTriangle.TriangleChecker(4,5,7));
+            Assert.AreEqual(testResult, Triangle.TriangleChecker(4,5,7));
         }
     }
 }
